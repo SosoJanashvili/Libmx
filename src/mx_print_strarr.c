46f7b6d7ca6c455d04166df49abcb01fdd1c7cdb
Soso Janashvili <sosojanashvili@gmail.com>
2020-07-30 12:39:42 +0400

    Updated function mx_print_strarr()

--- src/mx_print_strarr.c
+++ src/mx_print_strarr.c
@@ -2,19 +2,28 @@
 
 void mx_print_strarr(char **arr, const char *delim) {
 
+    int i = 0;
+    char ch;
+    int count = 0;
+
     if(!(*arr) || !delim)
         return;
 
-    int i = 0;
-    char ch = *delim;
-
-    for (; arr[i]; i++) {
-        mx_printstr(arr[i]);
-        mx_printchar(ch);
+    ch = *delim;
+    while (*(arr+count)) {
+        count++;
     }
 
+    while (i < count - 1) {
+        mx_printstr(arr[i]);
+        mx_printchar(ch);
+        i++;
+    }
     mx_printstr(arr[i]);
-
     mx_printchar('\n');
 }
 
+/* Code for main.c */
+//char *arr[] = {"Hello", "###", "There", "??", "!", "0", NULL, NULL, NULL, NULL};
+//char delim =  '_';
+//mx_print_strarr(arr, &delim);
