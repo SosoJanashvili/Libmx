2bb01aa4798f3c2d6b83ca06472c352b3c4dcad3
Soso Janashvili <sosojanashvili@gmail.com>
2020-08-03 23:54:53 +0400

    Updated function mx_push_front()

--- src/mx_push_front.c
+++ src/mx_push_front.c
@@ -2,9 +2,6 @@
 
 void mx_push_front(t_list **list, void *data) {
 
-    if (list == NULL || *list == NULL)
-        return;
-
     t_list *new = mx_create_node(data);
     new->next = *list;
     *list = new;
